af75b42f6c29abba73604907141fb026c7c87dba
Jay <jjayakumar@computer-talk.com>
2021-07-08 17:31:27 -0400

    Added Reporting Capabilities via Extent Reports Added Valid User Login Test case for Login Page

---
+++ SauceLabUIAutomation/LoginPage.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using System;
+
+namespace SauceLabUIAutomation
+{
+    public class LoginPage
+    {
+        private IWebDriver driver;
+
+        public LoginPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void GoTo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public HomePage EnterDetailsAndLogin(string v1, string v2)
+        {
+            return new HomePage(); 
+        }
+    }
+}
--- SauceLabUIAutomation/LoginPageTests.cs
+++ SauceLabUIAutomation/LoginPageTests.cs
@@ -1,18 +1,42 @@
+using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
 using NUnit.Framework;
+using OpenQA.Selenium;
 
 namespace SauceLabUIAutomation
 {
+    [TestFixture]
+    [Category("Login Variations")]
     public class LoginPageTests
     {
-        [SetUp]
-        public void Setup()
+        //Test Class Properties
+        public IWebDriver Driver { get; set; }
+        ExtentReports extent = null;
+
+        [OneTimeSetUp]
+        public void ExtentStart ()
         {
+            extent = new ExtentReports();
+            var htmlReporter = new ExtentHtmlReporter(@"C:\Users\jjayakumar\Desktop\Automation Projects\SauceLabUIAutomation\SauceLabUIAutomation\TestExecutionResults\LoginPageTestResults.html");
+            extent.AttachReporter(htmlReporter);
         }
 
-        [Test]
-        public void Test1()
+        [OneTimeTearDown]
+        public void ExtentClose()
         {
-            Assert.Pass();
+            extent.Flush();
+        }
+
+        [Description("Login with Standard User - Correct Username/Password")]
+        [Property("Author", "Jason Jayakumar")]
+        [Test]
+        public void SuccessfulLoginTest()
+        {
+            var loginPage = new LoginPage(Driver);
+            loginPage.GoTo();
+            var homePage = loginPage.EnterDetailsAndLogin("standard_user", "secret_sauce");
+            Assert.That(homePage.GetType(), Is.EqualTo(typeof(HomePage))); 
+
         }
     }
 }
